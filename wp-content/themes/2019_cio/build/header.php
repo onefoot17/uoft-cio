f1dc139d601656ee3db18247f37de59e5d25cdda
Earl Misquitta <e.misquitta@utoronto.ca>
2019-11-25 14:45:34 -0500

    Adjustments for mobile

--- wp-content/themes/2019_cio/build/header.php
+++ wp-content/themes/2019_cio/build/header.php
@@ -17,16 +17,16 @@
         <header id="header" class="section--header">
             <section class="section__width section__width--header">
                 <section class="section--header__logos">
-                    <a class="section--header__logos__uoft" href="https://www.utoronto.ca/" title="<?php echo get_bloginfo( 'name' ); ?>"><img class="section--header__logo" src="https://cio.utoronto.ca/wp-content/themes/ITS_uoft_bootstrap3_v203_sp/_inc/img/U-of-T-logo.svg" /></a>
+                    <a class="section--header__logos__uoft" href="https://www.utoronto.ca/" title=""><img class="section--header__logo" src="https://cio.utoronto.ca/wp-content/themes/ITS_uoft_bootstrap3_v203_sp/_inc/img/U-of-T-logo.svg" /></a>
 
-                    <a class="section--header__logos__cio" href="<?php echo get_site_url(); ?>" title="">
+                    <a class="section--header__logos__cio" href="<?php echo get_site_url(); ?>" title="<?php echo get_bloginfo( 'name' ); ?>">
                         <span class="section--header__logos__cio__top">Office of the</span>
 
-                        <span class="section--header__logos__cio__bottom">Chief<br />Information<br />Officer</span>
+                        <span class="section--header__logos__cio__bottom">Chief <br />Information<br />Officer</span>
                     </a>
 
-                    <section class="section--header__text">
-                        <img class="section--header__text__sign" src="https://cio.utoronto.ca/wp-content/uploads/2019/11/signature5dd462e16f1d7.png" />
+                    <section class="section--header__logos__text">
+                        <img class="section--header__logos__text__sign" src="https://cio.utoronto.ca/wp-content/uploads/2019/11/signature5dd462e16f1d7.png" />
 
                         <p>Bo Wandschneider, CIO</p>
 
